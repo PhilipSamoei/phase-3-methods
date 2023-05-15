de03c1fb21e78b400670ad2efa8dc73c73832c26
Philip Samoei <samphilkerich04@gmail.com>
2023-05-15 15:52:57 +0300

    complete code

--- methods.rb
+++ methods.rb
@@ -1,1 +1,36 @@
 # Your code here!
+def greet_programmer
+   puts "Hello, programmer!" 
+end
+
+greet_programmer
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+greet "Naureen"
+greet "Jimmy"
+
+def greet_with_default(name="programmer")
+    puts "Hello, #{name}!"
+end
+
+
+
+greet_with_default("Naureen")
+greet_with_default
+
+
+
+def add(num1, num2)
+    return num1 + num2
+    
+end
+sum = add(2, 5)
+
+def halve(number)
+    return nil unless number.is_a?(Integer)
+  
+    number / 2
+  end
+  
